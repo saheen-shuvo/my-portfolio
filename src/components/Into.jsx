c6d7b47a9533a2eec6ad310c3f2feff7c96021e0
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-04-07 02:32:53 +0600

    Navbar and Intro Restyled

--- src/components/Into.jsx
+++ src/components/Into.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import myFormalImg from "../assets/images/b&w me formal.jpg";
-import linkedInIcon from "../assets/images/linkedIn icon.png";
-import gitHubIcon from "../assets/images/gitHub icon1.png";
-import facebookIcon from "../assets/images/fb icon.png";
+import myFormalImg from "../assets/images/my-portfolio-wb.png";
+import { FaFacebookF, FaGithub } from "react-icons/fa";
+import { GrLinkedinOption } from "react-icons/gr";
 
 const Into = () => {
   return (
@@ -10,60 +9,61 @@ const Into = () => {
       id="home"
       className="hero pt-16 md:pt-28 bg-[#111111] max-w-screen-xl mx-auto"
     >
-      <div className="hero-content flex flex-row-reverse">
-        <img
-          src={myFormalImg}
-          className="w-[120px] md:w-64 rounded-lg shadow-2xl"
-        />
-        <div>
-          <h1 className="text-[15px] md:text-5xl font-bold text-white">
-            Hi, I'm Saheen Alam Shuvo
-          </h1>
-          <h1 className="text-[11px] md:text-3xl mt-1 md:mt-6 font-semibold text-white">
-            A Passionate MERN Stack Developer
-          </h1>
-          <p className="text-[8px] md:text-base text-justify mt-2 md:py-6 md:pr-10 text-gray-400">
-            I build modern, scalable web applications using React, Node.js,
-            MongoDB, and Express. Passionate about creating user-friendly
-            experiences and solving real-world problems with clean, efficient
-            code.
-          </p>
-          <div className="flex items-center gap-1 mt-1 md:mb-5">
-            {/* Facebook */}
-            <a href="https://www.facebook.com/share/1BYWbr36dS/?mibextid=wwXIfr">
-              {" "}
-              <button>
-                <img
-                  className="w-[26px] md:w-[50px]"
-                  src={facebookIcon}
-                  alt=""
-                />
+      <div id="intro-container" className="border-2  rounded-xl md:rounded-3xl lg:p-16 mx-2 md:mx-4">
+        <div className="hero-content flex flex-row-reverse px-2 md:px-4">
+          <img
+            id="intro-img"
+            src={myFormalImg}
+            className="w-24 md:w-56 lg:w-64 rounded-full"
+          />
+          <div className="sm:mr-8 md:mr-12 lg:mr-16">
+            <h1 className="text-[15px] md:text-5xl font-bold text-white">
+              Hi, I'm Saheen Alam Shuvo
+            </h1>
+            <h1 className="text-[11px] md:text-3xl mt-1 md:mt-6 font-semibold text-white">
+              A Passionate MERN Stack Developer
+            </h1>
+            <p className="text-[8px] md:text-base text-justify mt-2 md:py-4 md:pr-10 text-gray-400">
+              I build modern, scalable web applications using React, Node.js,
+              MongoDB, and Express. Passionate about creating user-friendly
+              experiences and solving real-world problems with clean, efficient
+              code.
+            </p>
+            <div className="flex items-center gap-1 md:gap-2 my-2 md:my-4 text-base md:text-lg lg:text-xl">
+              {/* Facebook */}
+              <a
+                className="social-btn text-[8px] md:text-lg p-1 lg:p-2 border-1 md:border-2 border-white"
+                href="https://www.facebook.com/share/1BYWbr36dS/?mibextid=wwXIfr"
+              >
+                <FaFacebookF />
+              </a>
+              {/* Github */}
+              <a
+                className="social-btn text-[8px] md:text-lg p-1 lg:p-2 border-1 md:border-2 border-white"
+                href="https://github.com/saheen-shuvo"
+              >
+                {" "}
+                <FaGithub />
+              </a>
+              {/* LinkedIn */}
+              <a
+                className="social-btn text-[8px] md:text-lg p-1 lg:p-2 border-1 md:border-2 border-white"
+                href="https://www.linkedin.com/in/saheen-alam-shuvo-182-li/"
+              >
+                <GrLinkedinOption />
+              </a>
+            </div>
+            <div className="mt-1 lg:mt-8">
+              <button id="intro-btn" className="text-[8px] md:text-[12px] px-[4px] md:px-[10px] py-[2px] md:py-[6px] font-semibold rounded-lg md:rounded-2xl">
+                Let's Connect
               </button>
-            </a>
-            {/* Github */}
-            <a href="https://github.com/saheen-shuvo">
-              {" "}
-              <button>
-                <img className="w-6 md:w-12" src={gitHubIcon} alt="" />
-              </button>
-            </a>
-            {/* LinkedIn */}
-            <a href="https://www.linkedin.com/in/saheen-alam-shuvo-182-li/">
-              <button>
-                <img className="w-6 md:w-12" src={linkedInIcon} alt="" />
-              </button>
-            </a>
-          </div>
-          <div className="mt-1">
-            <button className="bg-[#FFFFFF] btn-xs text-[#0D0D0D] hover:bg-[#E6E6E6] font-semibold px-2 py-1 md:px-4 md:py-2 rounded-md md:rounded-lg transition duration-300 mr-1 md:mr-2 text-[10px] md:text-[12px]">
-              Let's Connect
-            </button>
-            <a href="#projects">
-              {" "}
-              <button className="bg-[#FFFFFF] btn-xs text-[#0D0D0D] hover:bg-[#E6E6E6] font-semibold px-2 py-1 md:px-4 md:py-2 rounded-md md:rounded-lg transition duration-300 text-[10px] md:text-[12px]">
-                View Projects
-              </button>
-            </a>
+              <a href="#projects">
+                {" "}
+                <button id="intro-btn" className="ml-1 md:ml-2 text-[8px] md:text-[12px] px-[4px] md:px-[10px] py-[2px] md:py-[6px] font-semibold rounded-lg md:rounded-2xl">
+                  View Projects
+                </button>
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,65 +4,72 @@ const Navbar = () => {
   const links = (
     <>
       <li className="font-semibold">
-      <a href="#home">Home</a>
+        <a href="#home">Home</a>
       </li>
       <li className="font-semibold">
         <a href="#about">About</a>
       </li>
       <li className="font-semibold">
-      <a href="#skills">Skills</a>
+        <a href="#skills">Skills</a>
       </li>
       <li className="font-semibold">
-      <a href="#projects">Projects</a>
+        <a href="#projects">Projects</a>
       </li>
       <li className="font-semibold">
-      <a href="#contact">Contact</a>
+        <a href="#contact">Contact</a>
       </li>
     </>
   );
 
   return (
-    <div className="navbar fixed z-10 bg-opacity-30  bg-[#0d0d0d]">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div className="flex justify-center navbar bg-[#0d0d0d8d] fixed">
+      <div className="navbar fixed z-10 max-w-screen-xl">
+        {" "}
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost lg:hidden text-white"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            >
+              {links}
+            </ul>
           </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
-          >
-            {links}
-          </ul>
+          <div className="flex items-center gap-1 md:ml-4 ml-0">
+            <p className="text-xl font-bold text-green-800"></p>
+            <p className="text-sm md:text-xl font-bold  text-[white] flex items-center">
+              {" "}
+              Shuvo<span className="text-[#ff6a00]">X</span>Dev
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-1 md:ml-4 ml-0">
-          <p className="text-xl font-bold text-green-800"></p>
-          <p className="text-sm md:text-xl font-bold  text-[white] flex items-center">
-            {" "}
-            Shuvo<span className="text-[#ff6a00]">X</span>Dev
-          </p>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">{links}</ul>
         </div>
-      </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">{links}</ul>
-      </div>
-      <div className="navbar-end">
-        <button className="bg-[#FFFFFF] btn text-[#0D0D0D] hover:bg-[#E6E6E6] font-semibold px-4 py-2 rounded-lg transition duration-300 md:mr-4 mr-2">
-          Resume
-        </button>
+        <div className="navbar-end">
+          <button id="nav-btn" className="bg-[#FFFFFF] btn btn-sm md:btn-md text-[#0D0D0D] hover:bg-[#E6E6E6] font-semibold px-4 py-2 rounded-lg transition duration-300 md:mr-4 mr-2">
+            Resume
+          </button>
+        </div>
       </div>
     </div>
   );
