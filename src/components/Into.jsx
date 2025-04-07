48ca68cbae496c3b2691fcf7801610c94ee54a7c
Saheen Alam Shuvo <saheenshuvo182@gmail.com>
2025-04-07 14:23:48 +0600

    Scroll Smooth Added

--- src/components/Into.jsx
+++ src/components/Into.jsx
@@ -9,7 +9,10 @@ const Into = () => {
       id="home"
       className="hero pt-16 md:pt-28 bg-[#111111] max-w-screen-xl mx-auto"
     >
-      <div id="intro-container" className="border-2  rounded-xl md:rounded-3xl lg:p-16 mx-2 md:mx-4">
+      <div
+        id="intro-container"
+        className="border-2  rounded-xl md:rounded-3xl lg:p-16 mx-2 md:mx-4"
+      >
         <div className="hero-content flex flex-row-reverse px-2 md:px-4">
           <img
             id="intro-img"
@@ -20,8 +23,11 @@ const Into = () => {
             <h1 className="text-[15px] md:text-5xl font-bold text-white">
               Hi, I'm Saheen Alam Shuvo
             </h1>
-            <h1 className="text-[11px] md:text-3xl mt-1 md:mt-6 font-semibold text-white">
-              A Passionate MERN Stack Developer
+            <h1 className="intro-tags text-[11px] md:text-3xl mt-1 md:mt-6 font-semibold text-white">
+              A Dedicated <span style={{ "--i": 4 }} data-text="MERN Stack Developer">MERN Stack Developer</span>
+              <span style={{ "--i": 3 }} data-text="Frontend Developer">Frontend Developer</span>
+              <span style={{ "--i": 2 }} data-text="JavaScript Developer">JavaScript Developer</span>
+              <span style={{ "--i": 1 }} data-text="React Developer">React Developer</span>
             </h1>
             <p className="text-[8px] md:text-base text-justify mt-2 md:py-4 md:pr-10 text-gray-400">
               I build modern, scalable web applications using React, Node.js,
@@ -54,12 +60,18 @@ const Into = () => {
               </a>
             </div>
             <div className="mt-1 lg:mt-8">
-              <button id="intro-btn" className="text-[8px] md:text-[12px] px-[4px] md:px-[10px] py-[2px] md:py-[6px] font-semibold rounded-lg md:rounded-2xl">
+              <button
+                id="intro-btn"
+                className="text-[8px] md:text-[12px] px-[4px] md:px-[10px] py-[2px] md:py-[6px] font-semibold rounded-lg md:rounded-2xl"
+              >
                 Let's Connect
               </button>
               <a href="#projects">
                 {" "}
-                <button id="intro-btn" className="ml-1 md:ml-2 text-[8px] md:text-[12px] px-[4px] md:px-[10px] py-[2px] md:py-[6px] font-semibold rounded-lg md:rounded-2xl">
+                <button
+                  id="intro-btn"
+                  className="ml-1 md:ml-2 text-[8px] md:text-[12px] px-[4px] md:px-[10px] py-[2px] md:py-[6px] font-semibold rounded-lg md:rounded-2xl"
+                >
                   View Projects
                 </button>
               </a>
